09522d9cdbb3095f70040685b3b2e9d7716be599
sihle zungu <sihlemanzini10@gmail.com>
2023-06-19 06:55:26 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * maim - A program that prints the size of various computer type
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+print("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+print("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+print("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+print("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+print("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
