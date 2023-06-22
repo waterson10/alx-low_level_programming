2c2ffe7550e13033d5a62a9e2f1d4dd5884ceeda
sihle zungu <sihlemanzini10@gmail.com>
2023-06-22 04:26:20 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,12 +6,15 @@
  *
  * Return: 0 (Success)
  */
-
+ 
 int main(void)
 {
-char low;
-for (low = 'z'; low >= 'a'; low--);
-putchar(low);
-putchar('\n');
-return (0);
+	char low;
+	for (low = 'z', low >= 'a', low--)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+
 }
+
