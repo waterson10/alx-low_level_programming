c81c0f828aff44e9303bc1e9070b2b395af6b7fc
sihle zungu <sihlemanzini10@gmail.com>
2023-06-22 03:12:14 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char low, q, e;
+q = 'q';
+e  = 'e';
+
+for (low = 'a'; low <= 'z'; low++)
+{
+if (low != q && low != e)
+{
+putchar(low);
+}
+putchar('\n');
+}
+return (0);
+}
+
