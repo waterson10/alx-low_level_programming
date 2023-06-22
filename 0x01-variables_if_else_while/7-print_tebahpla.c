57bdc2b267e7c06861b2c8fee0b2544f65386576
sihle zungu <sihlemanzini10@gmail.com>
2023-06-22 04:13:48 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints alphabet in reverse
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+char low;
+for (low = 'z'; low >= 'a'; low--);
+putchar(low);
+putchar('\n');
+return (0);
+}
