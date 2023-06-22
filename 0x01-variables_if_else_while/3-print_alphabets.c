3432fcade2ba10a980906a310f7dafc64261c488
sihle zungu <sihlemanzini10@gmail.com>
2023-06-22 02:19:32 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabets in lowercase and then uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char low;
+
+for (low = 'a'; low <= 'z'; low++)
+{
+putchar(low);
+}
+for (low = 'A'; low <= 'Z'; low++)
+{
+putchar(low);
+putchar('\n');
+}
+return (0);
+}
