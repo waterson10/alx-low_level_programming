2da38450907df66fbeae3dcdb5004c68826a89cf
sihle zungu <sihlemanzini10@gmail.com>
2023-06-25 09:36:42 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabe_x10(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -10,4 +10,4 @@ void jack_burger(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
+void print_times_table(int ni);
