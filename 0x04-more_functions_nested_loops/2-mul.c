f0ef8be981098452be5226433a4837dabf4dc1b0
sihle zungu <sihlemanzini10@gmail.com>
2023-06-27 01:42:22 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiply two intergers
+ * @a: interger must be completed
+ * @b: second interger must be completed
+ * Return: a and b
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
