ab1ccbd7166c71f349ce5eec4840771825ec8020
sihle zungu <sihlemanzini10@gmail.com>
2023-06-29 05:48:33 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_array - Print an element of array of an interger 
+ * @a: Array of an interger
+ * @n: number of elements of an array of an interger
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+int j;
+
+for (j = 0; j < n; j++)
+{
+printf("%d", a[j]);
+if (j != (n - 1))
+{
+printf(", ");
+}
+}
+printf("\n");
+}
