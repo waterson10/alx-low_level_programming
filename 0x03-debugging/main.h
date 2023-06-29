766b8beaf3c7a5e019b164bb47fbf0c9dd29d3dc
sihle zungu <sihlemanzini10@gmail.com>
2023-06-29 03:37:58 -0700

    0-strcat

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,7 +4,8 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+int _putchar(char c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
-#endif
+#endif /* MAIn_H */
