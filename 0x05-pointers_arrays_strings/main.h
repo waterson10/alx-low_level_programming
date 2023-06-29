19ca8809a9cef07cad138d208f44e5c49b439974
sihle zungu <sihlemanzini10@gmail.com>
2023-06-29 02:48:20 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
+int _strlen(char *str);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -8,4 +8,3 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
